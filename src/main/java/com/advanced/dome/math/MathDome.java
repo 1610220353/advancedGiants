67e337b3e51db0613568cd031a03297f276335e4
guohong <yaoguohong@pansoft.com>
2020-06-19 15:17:06 +0800

    MD5生成器 String测试类 阿里线程池实现

---
+++ src/main/java/com/advanced/dome/math/MathDome.java
@@ -0,0 +1,16 @@
+package com.advanced.dome.math;
+
+/**
+ * @author guohong
+ * 用于测试 math 方法
+ */
+public class MathDome {
+
+    public static void main(String[] args) {
+
+        String a = "123";
+        int i = Integer.parseInt(a);
+
+        System.out.println(i);
+    }
+}
---
+++ src/main/java/com/advanced/dome/multithread/MyRunnable.java
@@ -0,0 +1,38 @@
+package com.advanced.dome.multithread;
+
+import java.util.Date;
+
+/**
+ * @author guohong
+ * @date 2020年5月27日17:32:43
+ * 创建 Runnable  用于测试 ThreadPoolExecutor
+ */
+public class MyRunnable implements Runnable{
+
+    private String command;
+
+    public MyRunnable(String s) {
+        this.command = s;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " Start. Time = " + new Date());
+        processCommand();
+        System.out.println(Thread.currentThread().getName() + " End. Time = " + new Date());
+    }
+
+    /**
+     * 模拟数据请求
+     */
+    public void processCommand() {
+
+        try {
+            Thread.sleep(10000);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
